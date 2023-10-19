aeade2f7ea77d29b4dd07f59c1bba6264020e0d7
Sumit Jamnani <sumit.jamnani@inferenz.ai>
2023-10-19 16:22:24 +0530

    Code Updated on 19-10-2023.

--- migrations/20231017122328-create-offers-and-discounts-applied.js
+++ migrations/20231017122328-create-offers-and-discounts-applied.js
@@ -3,7 +3,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable("offersAndDiscountsApplied", {
-      accountId: {
+      promotionId: {
         allowNull: false,
         primaryKey: true,
         type: Sequelize.UUID,
